e944329968c4308ffdf48aacd14c6d99105060e6
CHITRESH Kumar <chitreshkumar.bsr@gmail.com>
2023-02-10 12:50:42 +0530

    first commit

---
+++ add-to-cart/src/data.js
@@ -0,0 +1,31 @@
+import img1 from './img/img1.jpg';
+import img2 from './img/img2.jpg';
+import img3 from './img/img3.jpg';
+
+const data = {
+    productData:[
+        {
+            id :1,
+            img: img1,
+            title:'Chocolate',
+            desc:'',
+            price:5
+        },
+        {
+            id :2,
+            img: img2,
+            title:'Headphones',
+            desc:'',
+            price:25
+        },
+        {
+            id :3,
+            img: img3,
+            title:'Macbook M1',
+            desc:'',
+            price:680
+        },
+    ],
+};
+
+export default data;
